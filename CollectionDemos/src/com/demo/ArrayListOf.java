230fbb6680933e4c2acbfd705b0a9c85e4f5f6b7
unknown <swapnil.gajbhiye21@gmail.com>
2023-11-11 17:01:23 +0530

    added the arraylistof file

---
+++ CollectionDemos/src/com/demo/ArrayListOf.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListOf {
+
+	public static void main(String[] args) {
+		 
+		
+		List<String>list = Arrays.asList("swapnil","pankaj","sonal","snehal");
+		System.out.println(list);
+
+	}
+
+}
